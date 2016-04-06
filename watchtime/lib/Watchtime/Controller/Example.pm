d6ee251d8f3b519ae46c9ddac3eb88e48bf871bc
Dave Menninger <dave@menninger.io>
2016-04-06 13:40:44 -0400

    basic api functionality

--- watchtime/lib/Watchtime/Controller/Example.pm
+++ watchtime/lib/Watchtime/Controller/Example.pm
@@ -1,12 +1,37 @@
 package Watchtime::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Mojo::UserAgent;
+use Data::Dumper;
+
 # This action will render a template
 sub welcome {
   my $self = shift;
 
+  my $list = '<ul>';
+
+  my $trakt_api_key = $ENV{'TRAKT_API_KEY'} // 'nope';
+  my $ua   = Mojo::UserAgent->new;
+  my $json = $ua->get(
+      'https://api-v2launch.trakt.tv/users/sean/watchlist/shows' => {
+          'Content-Type'      => 'application/json',
+          'trakt-api-version' => '2',
+          'trakt-api-key'     => $trakt_api_key,
+      }
+  )->res->json;
+
+  for my $show ( @{$json} )
+  {
+    say "a show:";
+    say $show->{'show'}->{'title'};
+    $list .= '<li>'.$show->{'show'}->{'title'}.'</li>';
+    say Dumper( $show );
+  }
+
+  $list .= '</ul>';
+
   # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!', list => $list);
 }
 
 1;
